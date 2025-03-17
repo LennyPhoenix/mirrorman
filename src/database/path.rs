197c57cf124cf585e32ffc81665a69eefe8cd3aa
Lenny Critchley <LennyPhoenixC@gmail.com>
2025-03-17 13:57:49 +0000

    Make database names nicer
    Resolves #4

--- src/database/path.rs
+++ src/database/path.rs
@@ -8,8 +8,12 @@ pub fn database_path_from_mirror(mirror_path: &Path) -> Result<PathBuf> {
     let path = mirror_path
         .components()
         .filter_map(|c| c.as_os_str().to_ascii_lowercase().into_string().ok())
-        .reduce(|a, b| format!("{0}_{1}", a, b))
-        .with_context(|| "Failed to build database filename")?;
+        .reduce(|a, b| format!("{0} {1}", a, b))
+        .with_context(|| "Failed to build database filename")?
+        .replace("/", " ")
+        .replace(".", " ")
+        .trim()
+        .replace(" ", "_");
 
     let path = PathBuf::from_str(&format!("{path}.mmdb"))
         .with_context(|| "Failed to construct database path from mirror path")?;
